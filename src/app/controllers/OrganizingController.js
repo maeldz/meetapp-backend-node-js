1434da1d733475b5d6465fd91c6bc63d4b36b5ea
Tomb Raider Evolution <mael.dazareia@gmail.com>
2019-09-30 16:23:42 -0300

    update organizing controller

--- src/app/controllers/OrganizingController.js
+++ src/app/controllers/OrganizingController.js
@@ -1,11 +1,18 @@
+import { Op } from "sequelize";
+
 import Meetup from "../models/Meetup";
 import File from "../models/File";
 
 class OrganizingController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const meetups = await Meetup.findAll({
       where: {
-        user_id: req.userId
+        user_id: req.userId,
+        date: {
+          [Op.gt]: new Date()
+        }
       },
       include: [
         {
@@ -13,10 +20,16 @@ class OrganizingController {
           as: "banner",
           attributes: ["id", "path", "url"]
         }
-      ]
+      ],
+      limit: 5,
+      offset: (page - 1) * 5
     });
 
-    return res.json(meetups);
+    let pages = await Meetup.count({ where: { user_id: req.userId } });
+
+    pages = Math.ceil(pages / 5);
+
+    return res.json({ meetups: [...meetups], pages: pages });
   }
 }
 
